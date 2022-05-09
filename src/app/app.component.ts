32a3f2af95af55f3197be67f96fa8cdfc1932203
Giovanny1991 <alexge2009@hotmail.com>
2022-05-09 00:00:03 -0500

    actualizando 05082022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent {
-  title = 'DeberSemana2';
+  
+  title = 'EXAMEN';
+  edad = 15;
+  mostrar = true;
+  nombre="Nombre";
+
+  usuarios = [
+    {
+      nombre: 'Alex',
+      apellido: 'Capa'
+    },
+    {
+      nombre: 'Alejandra',
+      apellido: 'Tipan'
+    }
+  ]
+
+  validarEdad() {
+
+    if (this.edad >= 18) {
+
+      return "Eres mayor de Edad";
+
+    } else {
+      return "Eres menor de Edad";
+    }
+  }
+
+  mostrarTexto() {
+
+    this.mostrar = !this.mostrar;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
